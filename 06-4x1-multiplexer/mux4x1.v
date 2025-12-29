1b6b01d959108ca06ca4be7f785ad34b5eee77fc
Fareed <fareed1983@gmail.com>
2025-12-29 11:33:15 +1100

    simplified a little

--- 06-4x1-multiplexer/mux4x1.v
+++ 06-4x1-multiplexer/mux4x1.v
@@ -6,13 +6,12 @@ module mux4x1
 	);
 	
 	wire[3:0] a;
-	wire [1:0] ns = ~s;
 	
-	assign a[0] = i[0] & ns[1] & ns[0];
-	assign a[1] = i[1] & ns[1] &  s[0];
-	assign a[2] = i[2] &  s[1] & ns[0];
+	assign a[0] = (i[0] & !s[1] & !s[0]);
+	assign a[1] = (i[1] & !s[1] &  s[0]);
+	assign a[2] = (i[2] &  s[1] & !s[0];
 	assign a[3] = i[3] &  s[1] &  s[0];
 	
-	assign y = a[0] | a[1] | a[2] | a[3];
+	assign y = (a[0] | a[1] | a[2] | a[3]);
 	
 endmodule
