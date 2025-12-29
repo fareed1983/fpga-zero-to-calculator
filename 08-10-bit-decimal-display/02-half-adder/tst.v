fe08f1f3750c025638eb7b9e1ee47f6d58713652
Fareed <fareed1983@gmail.com>
2025-12-29 13:15:26 +1100

    removing folders added by mistake

--- 08-10-bit-decimal-display/02-half-adder/tst.v
+++
@@ -1,9 +0,0 @@
-module tst
-	(
-		input wire[1:0] SW,
-		output wire[1:0] LEDR
-	);
-	
-	ha ha1 (.a(SW[0]), .b(SW[1]), .s(LEDR[0]), .c(LEDR[1]));
-	
-endmodule
--- 08-10-bit-decimal-display/03-full-adder/tst.v
+++
@@ -1,9 +0,0 @@
-module tst
-	(
-		input wire[0:2] SW,
-		output wire[0:1] LEDR
-	);
-	
-	fa fa1 (.a(SW[0]), .b(SW[1]), .c(SW[2]), .sum(LEDR[0]), .carry(LEDR[1]));
-	
-endmodule
