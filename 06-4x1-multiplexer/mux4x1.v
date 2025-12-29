596011da7ecf0021302643f053fc54d2a648534b
Fareed <fareed1983@gmail.com>
2025-12-29 11:33:58 +1100

    typo

--- 06-4x1-multiplexer/mux4x1.v
+++ 06-4x1-multiplexer/mux4x1.v
@@ -9,8 +9,8 @@ module mux4x1
 	
 	assign a[0] = (i[0] & !s[1] & !s[0]);
 	assign a[1] = (i[1] & !s[1] &  s[0]);
-	assign a[2] = (i[2] &  s[1] & !s[0];
-	assign a[3] = i[3] &  s[1] &  s[0];
+	assign a[2] = (i[2] &  s[1] & !s[0]);
+	assign a[3] = (i[3] &  s[1] &  s[0]);
 	
 	assign y = (a[0] | a[1] | a[2] | a[3]);
 	
